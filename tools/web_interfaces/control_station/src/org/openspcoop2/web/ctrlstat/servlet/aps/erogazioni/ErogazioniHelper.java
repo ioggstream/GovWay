8de958bd7eec1cbc38a4d2356b400ca1f8e10bd2
Giuliano Pintori <pintori@link.it>
2018-10-12 16:37:16 +0200

    fix link api.

--- tools/web_interfaces/control_station/src/org/openspcoop2/web/ctrlstat/servlet/aps/erogazioni/ErogazioniHelper.java
+++ tools/web_interfaces/control_station/src/org/openspcoop2/web/ctrlstat/servlet/aps/erogazioni/ErogazioniHelper.java
@@ -78,6 +78,7 @@ import org.openspcoop2.web.ctrlstat.core.Search;
 import org.openspcoop2.web.ctrlstat.costanti.CostantiControlStation;
 import org.openspcoop2.web.ctrlstat.servlet.apc.AccordiServizioParteComuneCostanti;
 import org.openspcoop2.web.ctrlstat.servlet.apc.AccordiServizioParteComuneUtilities;
+import org.openspcoop2.web.ctrlstat.servlet.apc.api.ApiCostanti;
 import org.openspcoop2.web.ctrlstat.servlet.aps.AccordiServizioParteSpecificaCostanti;
 import org.openspcoop2.web.ctrlstat.servlet.aps.AccordiServizioParteSpecificaHelper;
 import org.openspcoop2.web.ctrlstat.servlet.archivi.ExporterUtils;
@@ -1031,7 +1032,7 @@ public class ErogazioniHelper extends AccordiServizioParteSpecificaHelper{
 			de.addTarget(TargetType.SELF);
 		}
 		
-		de.addUrl(AccordiServizioParteComuneCostanti.SERVLET_NAME_APC_CHANGE, 
+		de.addUrl(ApiCostanti.SERVLET_NAME_APC_API_CHANGE, 
 				new Parameter(AccordiServizioParteSpecificaCostanti.PARAMETRO_APS_ID, asps.getIdAccordo() + ""),
 				new Parameter(AccordiServizioParteSpecificaCostanti.PARAMETRO_APS_NOME, as.getNome()), pTipoAccordo);
 		de.addToolTip(MessageFormat.format(ErogazioniCostanti.ASPS_EROGAZIONI_ICONA_VISUALIZZA_TOOLTIP_CON_PARAMETRO, AccordiServizioParteSpecificaCostanti.LABEL_APC_COMPOSTO_SOLO_PARTE_COMUNE));
